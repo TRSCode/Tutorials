8e32ecbd20601fac195e19197af786caf015d6ff
Tony Schock <tony.schock@gmail.com>
2023-09-11 17:27:20 -0600

    update

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
-const API_Key = "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";
+const API_Key = "";
 
 function App() {
   const [typing, setTyping] = useState(false)
