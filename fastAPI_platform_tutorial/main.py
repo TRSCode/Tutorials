f76d74530b2e646b53064cbe8d99a2120bcbe271
Tony Schock <tony.schock@gmail.com>
2023-10-04 15:16:00 -0600

    paths added

--- fastAPI_platform_tutorial/main.py
+++ fastAPI_platform_tutorial/main.py
@@ -1,1 +1,34 @@
-# to run -> uvicorn main:app --reload
+# to run -> uvicorn main:app --reload (main is the file name and app is the object name)
+# view docs -> localhost:8000/docs (swagger UI)
+# view redoc -> localhost:8000/redoc (redoc UI)
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+# async def read_item(item_id):
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
