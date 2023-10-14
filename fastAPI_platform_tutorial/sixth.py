84eeb7826f1561706106058a1381e8a7e1ecaed6
Tony Schock <tony.schock@gmail.com>
2023-10-13 23:02:39 -0600

    completed declare request example data

--- fastAPI_platform_tutorial/sixth.py
+++ fastAPI_platform_tutorial/sixth.py
@@ -30,20 +30,133 @@ async def root():
 
 # --- now a quicker way using Field and examples providing a similar JSON return ---
 
+# class Item(BaseModel):
+#     name: str = Field(examples=["Foo"])
+#     description: str | None = Field(default=None, examples=["A very nice Item"])
+#     price: float = Field(examples=[35.4])
+#     tax: float | None = Field(default=None, examples=[3.2])
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: int,
+#     item: Annotated[
+#         Item,
+#         Body(
+#             examples=[
+#                 {
+#                     "name": "Foo",
+#                     "description": "A very nice Item",
+#                     "price": 35.4,
+#                     "tax": 3.2,
+#                 }
+#             ],
+#         ),
+#     ],
+# ):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# --- this example shows how to use multiple examples, but is not supported by Swagger UI ---
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int,
+#     item: Annotated[
+#         Item,
+#         Body(
+#             examples=[
+#                 {
+#                     "name": "Foo",
+#                     "description": "A very nice Item",
+#                     "price": 35.4,
+#                     "tax": 3.2,
+#                 },
+#                 {
+#                     "name": "Bar",
+#                     "price": "35.4",
+#                 },
+#                 {
+#                     "name": "Baz",
+#                     "price": "thirty five point four",
+#                 },
+#             ],
+#         ),
+#     ],
+# ):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# --- this example shows how to use multiple examples, but is supported by Swagger UI (see dropdown in Swagger UI) ---
+
 class Item(BaseModel):
-    name: str = Field(examples=["Foo"])
-    description: str | None = Field(default=None, examples=["A very nice Item"])
-    price: float = Field(examples=[35.4])
-    tax: float | None = Field(default=None, examples=[3.2])
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(
+    *,
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
     results = {"item_id": item_id, "item": item}
     return results
