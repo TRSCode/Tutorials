cb5e6bcef52a6968816c9b92f0bbab64b24e6473
Tony Schock <tony.schock@gmail.com>
2023-10-17 17:00:09 -0600

    started extra models

---
+++ fastAPI_platform_tutorial/nineth.py
@@ -0,0 +1,14 @@
+from typing import Annotated, Union, List, Any
+from fastapi import FastAPI, Path, Query, Body, Cookie, Header, Response
+from fastapi.responses import JSONResponse, RedirectResponse
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from datetime import datetime, time, timedelta
+from typing import Annotated
+from uuid import UUID
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Extra Models"}
