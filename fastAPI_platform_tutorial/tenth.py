5f189f375f3cd6de70eefa889ef3928f24388241
Tony Schock <tony.schock@gmail.com>
2023-10-19 11:14:53 -0600

    form data

--- fastAPI_platform_tutorial/tenth.py
+++ fastAPI_platform_tutorial/tenth.py
@@ -1,5 +1,5 @@
 from typing import Annotated, Union, List, Any
-from fastapi import FastAPI, Path, Query, Body, Cookie, Header, Response, status
+from fastapi import FastAPI, Path, Query, Body, Cookie, Header, Response, status, Form
 from fastapi.responses import JSONResponse, RedirectResponse
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
 from datetime import datetime, time, timedelta
@@ -11,8 +11,14 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Response Status Code"}
+    return {"message": "Response Status Code and Form Data"}
 
 @app.post("/items/", status_code=status.HTTP_201_CREATED)
 async def create_item(name: str):
     return {"name": name}
+
+# --- install and import python-multipart  ---
+
+@app.post("/login/", status_code=201)
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
