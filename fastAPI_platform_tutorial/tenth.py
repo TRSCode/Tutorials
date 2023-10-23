1dd8a12686df39db242aa774dea0da975939390f
Tony Schock <tony.schock@gmail.com>
2023-10-22 19:53:39 -0600

    request form and files

--- fastAPI_platform_tutorial/tenth.py
+++ fastAPI_platform_tutorial/tenth.py
@@ -11,23 +11,40 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Response Status Code, Form Data, Request Files"}
+    return {"message": "Response Status Code, Form Data, Request Files, Request Forms and Files"}
 
-@app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(name: str):
-    return {"name": name}
+# @app.post("/items/", status_code=status.HTTP_201_CREATED)
+# async def create_item(name: str):
+#     return {"name": name}
 
 # --- install and import python-multipart  ---
 
-@app.post("/login/", status_code=201)
-async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"username": username}
+# @app.post("/login/", status_code=201)
+# async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+#     return {"username": username}
+
+# @app.post("/files/")
+# async def create_file(file: Annotated[bytes, File()]):
+#     return {"file_size": len(file)}
+
+
+# @app.post("/uploadfile/")
+# async def create_upload_file(file: UploadFile):
+#     return {"filename": file.filename}
+
+# --- Request Forms and Files ---
+
+app = FastAPI()
+
 
 @app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+async def create_file(
+    file: Annotated[bytes, File()],
+    fileb: Annotated[UploadFile, File()],
+    token: Annotated[str, Form()],
+):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
