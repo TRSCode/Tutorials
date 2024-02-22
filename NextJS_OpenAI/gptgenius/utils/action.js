245de289d24bc143b3d82a3322741c4d988e370c
Tony Schock <tony.schock@gmail.com>
2024-02-22 15:46:32 -0700

    through lesson 77

--- NextJS_OpenAI/gptgenius/utils/action.js
+++ NextJS_OpenAI/gptgenius/utils/action.js
@@ -1,6 +1,27 @@
 'use server'
+import OpenAI from 'openai';
 
-export const generateChatResponse = async (chatMessage) => {
-    console.log(chatMessage);
-    return 'awesome';
-}
+const openai = new OpenAI({
+    apiKey:process.env.OPENAI_API_KEY
+});
+
+export const generateChatResponse = async (chatMessages) => {
+    // console.log(chatMessage);
+    try {
+        const response = await openai.chat.completions.create({
+            messages:[
+                {role:'system',content:'You are a helpful assistant.'},
+                // {role:'user',content:chatMessage}
+                ...chatMessages,
+            ],
+            model:"gpt-3.5-turbo",
+            temperature:0,
+        });
+        // console.log(response.choices[0].message);
+        // console.log(response);
+        return response.choices[0].message;
+
+    } catch (error) {
+        return null;
+    }
+};
