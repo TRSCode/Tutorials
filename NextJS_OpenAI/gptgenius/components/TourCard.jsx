4d40024d6c106d144e21c5fa60fad869864c9b3d
Tony Schock <tony.schock@gmail.com>
2024-02-24 19:30:45 -0700

    fix tour list top margin

--- NextJS_OpenAI/gptgenius/components/TourCard.jsx
+++ NextJS_OpenAI/gptgenius/components/TourCard.jsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 const TourCard = ({tour}) => {
     const {city, id, country} = tour;
     return (
-        <Link href={`/tours/${id}`} className="card card-compact rounded-xl bg-base-100" >
-            <div className="card-body items-center text-center">
-                <h2 className="card-title text-center">
-                    {city}, {country}
-                </h2>
-            </div>
-        </Link>
+            <Link href={`/tours/${id}`} className="card card-compact rounded-xl bg-base-100" >
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title text-center">
+                        {city}, {country}
+                    </h2>
+                </div>
+            </Link>
     )
 }
 
--- NextJS_OpenAI/gptgenius/components/ToursList.jsx
+++ NextJS_OpenAI/gptgenius/components/ToursList.jsx
@@ -4,7 +4,7 @@ const ToursList = ({ data }) => {
     if(data.lenght === 0) return <h4 className="text-lg">No tours found...</h4>
 
     return (
-        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8" >
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-5" >
             {data.map((tour)=>{
                 return <TourCard key={tour.id} tour={tour} />
             })}
