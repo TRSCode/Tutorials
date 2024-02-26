fb82f2eaa4605319d69cb2effc33f78abc7bd04b
Tony Schock <tony.schock@gmail.com>
2024-02-26 10:24:28 -0700

    complete through 94

--- NextJS_OpenAI/gptgenius/app/(dashboard)/tours/[id]/page.js
+++ NextJS_OpenAI/gptgenius/app/(dashboard)/tours/[id]/page.js
@@ -1,6 +1,7 @@
-import { getSingleTour } from "@/utils/action"
+import { generateTourImage, getSingleTour } from "@/utils/action"
 import { redirect } from "next/navigation"
 import Link from "next/link"
+import Image from "next/image"
 import TourInfo from "@/components/TourInfo"
 
 const SingleTourPage = async ({ params }) => {
@@ -9,12 +10,29 @@ const SingleTourPage = async ({ params }) => {
     if(!tour) {
     redirect('/tours');
     }
+    
+    const tourImage = await generateTourImage({
+        city: tour.city,
+        country: tour.country,
+    });
 
     return (
         <div>
             <Link href="/tours" className="btn btn-secondary mb-12">
                 back to tours
             </Link>
+            {tourImage ? (
+                <div>
+                    <Image
+                        src={tourImage}
+                        width={300}
+                        height={300}
+                        className='rounded-xl shadow-xl mb-16 h-96 w-96 object-cover'
+                        alt={tour.title}
+                        priority
+                    />
+                </div>
+            ) : null}
             <TourInfo tour={tour} />
         </div>
     )
--- NextJS_OpenAI/gptgenius/next.config.mjs
+++ NextJS_OpenAI/gptgenius/next.config.mjs
@@ -1,4 +1,50 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+//                 port: '',
+//                 pathname: '/private/**',
+//             },
+//             {
+//                 protocol: 'https',
+//                 hostname: 'images.unsplash.com',
+//                 port: '',
+//                 pathname: '/**',
+//             },
+//         ],
+//     },
+// };
+
+// export default nextConfig;
+
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+                port: '',
+                pathname: '/private/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
