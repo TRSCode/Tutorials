67bd0bac5cadaad5bd6ef6e3614f629da3510855
Tony Schock <tony.schock@gmail.com>
2024-03-08 11:31:40 -0700

    complete through 132

--- NextJS_OpenAI/jobify/components/JobsList.tsx
+++ NextJS_OpenAI/jobify/components/JobsList.tsx
@@ -1,6 +1,37 @@
+'use client';
+import JobCard from './JobCard';
+import { useSearchParams } from 'next/navigation';
+import { getAllJobsAction } from '@/utils/actions';
+import { useQuery } from '@tanstack/react-query';
+
 function JobsList() {
+    const searchParams = useSearchParams();
+
+    const search = searchParams.get('search') || '';
+    const jobStatus = searchParams.get('jobStatus') || 'all';
+
+    const pageNumber = Number(searchParams.get('page')) || 1;
+
+    const {data, isPending} = useQuery({
+        queryKey:['jobs', search, jobStatus, pageNumber],
+        queryFn: () => getAllJobsAction({search, jobStatus, pageNumber})
+    })
+
+    const jobs = data?.jobs || [];
+
+    if (isPending) return <h2 className='text-xl'>Please Waite...</h2>
+
+    if (jobs.length < 1) return <h2 className='text-xl'>No Jobs Found...</h2>
+
     return (
-        <h1 className="text-4xl">JobsList</h1>
+        <>
+            {/* button container */}
+            <div className='grid md:grid-col-2 gap-8'>
+                {jobs.map((job)=>{
+                    return <JobCard key={job.id} job={job} />
+                })}
+            </div>
+        </>
     )
 }
 
